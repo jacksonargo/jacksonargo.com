ac7f4962d990f7b4e8624d2b6ddc3e9bd49dfc94
Jackson Argo <jackson@jacksonargo.com>
2017-10-04 20:49:56 -0700

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,3 @@
-rsync -au public_html deploy@api.jacksonargo.com:/var/www/html/www.jacksonargo.com
+#!/bin/bash
+set -x
+rsync -auP public_html deploy@api.jacksonargo.com:/var/www/html/www.jacksonargo.com
